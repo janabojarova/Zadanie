12bc045b8d39f5dc106d106de6169c9018ce2c17
Jana Bojarova <thejana13@gmail.com>
2022-04-02 17:02:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter your exam result");
+            char userInput = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch(userInput)
+            {
+                case 'A':
+                    Console.WriteLine("Outstanding");
+                    break;
+
+                case 'B':
+                    Console.WriteLine("Superior");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                    Console.WriteLine("Satisfactory");
+                    break;
+                case 'E':
+                    Console.WriteLine("Low Pass");
+                    break;
+                case 'F':
+                    Console.WriteLine("Failure");
+                    break;
+                    
+
+
+
+              
+
+
+            }
+
+        }
+    }
+}
